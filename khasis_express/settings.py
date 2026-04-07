41f37db963d4f5163a3e8be645edfa7acea32361
Xamy9 <samueleneh37@gmail.com>
2026-04-07 10:49:23 +0100

    Added PostgresSQL database

--- khasis_express/settings.py
+++ khasis_express/settings.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from pathlib import Path
-
+import dj_database_url
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
@@ -105,10 +105,9 @@ WSGI_APPLICATION = 'khasis_express.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("postgresql://khasis_db_user:KbdvrrUvI46kd72jVqBOjdQsoPPxf9qr@dpg-d7a1u6udqaus73emhop0-a/khasis_db")
+    )
 }
 
 
